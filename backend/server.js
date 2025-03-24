805b183cbb2eb600e1118ff74115a355cde2d1dd
abhixsh <aloka.abishek9@gmail.com>
2025-03-24 14:21:36 +0530

    Initial setup

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+// server/server.js
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+// Serve the React app for any unknown routes
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
